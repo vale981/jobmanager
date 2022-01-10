2e7d508bbd8732aa2e6a1bf575c5d3d68c86183a
Valentin Boettcher <hiro@protagon.space>
2022-01-10 15:18:16 +0100

    reuse address to fix [Errno 98]

--- jobmanager/jobmanager.py
+++ jobmanager/jobmanager.py
@@ -1522,6 +1522,7 @@ class JobManager_Server(object):
     def _check_bind(host, port):
         try:
             s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
             s.bind((host, port))
         except:
             log.critical("test bind to %s:%s failed", host, port)
